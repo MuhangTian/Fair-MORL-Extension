1. Added visualization for evaluation in RG. 2. Made sure R_acc is at least during computation in RAVI. 3. Fixed seeding for RG

algo/ra_value_iteration.py
@@ -12,6 +12,7 @@ sys.path.insert(0, '../envs')
 import envs
 
 import os
+import pdb
 
 class RAValueIteration:
     def __init__(self, env, discre_alpha, growth_rate, gamma, reward_dim, time_horizon, welfare_func_name, nsw_lambda, save_path, seed=1122, p=None, threshold=5, wdb=False, scaling_factor=1) -> None:
@@ -38,7 +39,7 @@ class RAValueIteration:
             max_reward = self.time_horizon/self.scaling_factor
         else:
             # Calculate the sum of the geometric series
-            sum_of_discounts = (1 - self.gamma ** (self.time_horizon/self.scaling_factor)) / (1 - self.gamma)
+            sum_of_discounts = (1 - self.gamma ** ((self.time_horizon+1)/self.scaling_factor)) / (1 - self.gamma)
             max_reward = sum_of_discounts
         max_discrete = self.discre_func(max_reward)
 
@@ -67,7 +68,7 @@ class RAValueIteration:
         for Racc in tqdm(self.init_Racc, desc="Initializing..."):
             for state in range(self.env.observation_space.n):
                 Racc_code = self.encode_Racc(Racc)
-                self.V[state, Racc_code, 0] = self.welfare_func(self.gamma ** self.time_horizon * Racc)
+                self.V[state, Racc_code, 0] = self.welfare_func(Racc)
     
     def encode_Racc(self, Racc):
         # Encode the accumulated reward for indexing.
@@ -84,13 +85,19 @@ class RAValueIteration:
             # Use the discretized grid from initialization that accounts for exponential growth
             # Calculate the sum of the geometric series for the remaining timesteps
             remaining_steps = self.time_horizon - t
-            max_accumulated_discounted_reward = (1 - self.gamma ** (remaining_steps)) / (1 - self.gamma)
+            if self.gamma == 1:
+                max_accumulated_discounted_reward = remaining_steps
+            else:
+                max_accumulated_discounted_reward = (1 - self.gamma ** (remaining_steps+1)) / (1 - self.gamma)
 
             max_possible_reward = min(max_accumulated_discounted_reward, self.discre_grid[-2])
             
             # max_possible_reward = np.round(max_accumulated_discounted_reward / self.discre_alpha) * self.discre_alpha / scaling_factor
 
             max_possible_discretized_reward = self.discre_func(max_possible_reward)
+
+            while max_possible_discretized_reward < 1:
+                max_possible_discretized_reward = self.discre_func(max_possible_discretized_reward+self.discre_alpha)
 
             print()
             print(f"Max discretized reward at t = {remaining_steps}: {max_possible_discretized_reward}")
@@ -121,8 +128,13 @@ class RAValueIteration:
         np.savez(self.save_path, V=self.V, Pi=self.Pi, Racc_record=self.Racc_record)
     
     def evaluate(self, final=False):
-        self.env.seed(self.seed)
-        state = self.env.reset()
+        # self.env.seed(self.seed)
+        state = self.env.reset(seed=self.seed)
+        # Ensure the renders directory exists within the specified save path
+        renders_path = self.save_path + '/renders'
+        os.makedirs(renders_path, exist_ok=True)
+        img_path = self.save_path + f'/renders/env_render_init.png'
+        self.env.render(save_path=img_path)
         Racc = np.zeros(self.reward_dim)
         c = 0
         
@@ -132,16 +144,13 @@ class RAValueIteration:
             
             next, reward, done = self.env.step(action)
             if isinstance(self.env, envs.Resource_Gathering.ResourceGatheringEnv):
-                # Ensure the renders directory exists within the specified save path
-                renders_path = self.save_path + '/renders'
-                os.makedirs(renders_path, exist_ok=True)
                 img_path = self.save_path + f'/renders/env_render_{self.time_horizon-t}.png'
                 self.env.render(save_path=img_path)
                 # decoded_pos, decoded_status = self.env.decode_state(next)
                 # print(f"Decoded Position: {decoded_pos}, Decoded Resource Status: {decoded_status}")
             state = next
             Racc += np.power(self.gamma, c) * reward
-            print(f"Accumulated Reward: {Racc}")
+            print(f"Accumulated Reward at t = {self.time_horizon-t}: {Racc}")
             
             c += 1
         

algo/utils.py
@@ -15,11 +15,14 @@ class DiscreFunc:
         if not isinstance(x, np.ndarray):
             x = np.array([x])  # Convert scalar to numpy array if not already an array
         assert isinstance(x, np.ndarray), "x must be a numpy array"
-        scaled_x = x / self.alpha
-        indices = np.round(np.log1p(scaled_x * (self.growth_rate - 1)) / np.log(self.growth_rate)).astype(int)
-        # Calculate the accumulated discretization bins
-        bins = self.alpha * (self.growth_rate ** indices - 1) / (self.growth_rate - 1)
-        return bins
+        if self.growth_rate > 1: 
+            scaled_x = x / self.alpha
+            indices = np.round(np.log1p(scaled_x * (self.growth_rate - 1)) / np.log(self.growth_rate)).astype(int)
+            # Calculate the accumulated discretization bins
+            bins = self.alpha * (self.growth_rate ** indices - 1) / (self.growth_rate - 1)
+            return bins
+        else:
+            return np.round(x / self.alpha) * self.alpha
 
 class WelfareFunc:
     def __init__(self, welfare_func_name, nsw_lambda=None, p=None, threshold=5):
@@ -60,11 +63,17 @@ class WelfareFunc:
         return np.power(np.prod(x), 1 / len(x))
 
     def resource_damage_scalarization(self, x):
+        # assert isinstance(x, np.ndarray), "x must be a numpy array"
+        # assert len(x) == 2, "x must have length 2 (resources_collected, damage_taken)"
+        # R = x[0]
+        # D = x[1]
+        # return R - max(0, (D - self.threshold) ** 3)
         assert isinstance(x, np.ndarray), "x must be a numpy array"
         assert len(x) == 2, "x must have length 2 (resources_collected, damage_taken)"
         R = x[0]
         D = x[1]
-        return R - max(0, (D - self.threshold) ** 3)
+        beta = 1 - self.threshold
+        return (R ** self.threshold) * ((1 / (D+1)) ** beta)
 
 def is_file_on_disk(file_name):
     if not os.path.isfile(file_name):
@@ -106,8 +115,8 @@ if __name__ == "__main__":
     # print("Discretized Output:", result)
 
     # Example usage
-    x = np.array([4, 0])  # Example reward vector [resources_collected, damage_taken]
-    threshold = 2  # Example threshold
+    x = np.array([2.3661, 1.38742049])  # Example reward vector [resources_collected, damage_taken]
+    threshold = 0.5  # Example threshold
     wf = WelfareFunc(welfare_func_name="resource_damage_scalarization", threshold=threshold)
     result = wf(x)
     print(f"Scalarized reward: {result}")

train.py
@@ -44,7 +44,7 @@ def parse_arguments():
     prs.add_argument('--num_locs', type=is_positive_integer, default=2)
     prs.add_argument('--time_horizon', type=is_positive_integer, default=1)
     prs.add_argument('--discre_alpha', type=is_positive_float, default=0.5)
-    prs.add_argument("--gamma", type=is_within_zero_one_float, default=0.999)
+    prs.add_argument("--gamma", type=is_positive_float, default=0.999)
     prs.add_argument("--growth_rate", type=is_positive_float, default=1.001)
     prs.add_argument("--welfare_func_name", choices=["egalitarian", "nash-welfare", "p-welfare", "resource_damage_scalarization"], default="nash-welfare")
     prs.add_argument("--nsw_lambda", type=is_positive_float, default=1e-4)
@@ -57,7 +57,7 @@ def parse_arguments():
     prs.add_argument("--init_val", type=float, default=0.0)
     prs.add_argument("--dim_factor", type=is_positive_float, default=0.9)
     prs.add_argument("--p", type=float, default=0.5)
-    prs.add_argument("--threshold", type=int, default=5)  # For resource_damage_scalarization
+    prs.add_argument("--threshold", type=is_positive_float, default=0.5)  # For resource_damage_scalarization
     prs.add_argument("--scaling_factor", type=int, default=1) # scaling factor for accumuated reward initialization in RA-Value Iteration, should be 14 for taxi
     prs.add_argument("--num_resources", type=int, default=3) # number of resources in Resource Gathering
     prs.add_argument("--project", type=str, default="RA-Iteration")
@@ -76,7 +76,7 @@ if __name__ == "__main__":
         size, loc_coords, dest_coords = get_setting(args.size, args.num_locs)
         env = Fair_Taxi_MOMDP(size, loc_coords, dest_coords, args.time_horizon, '', 15)
     elif args.env_name == "ResourceGatheringEnv":
-        env = ResourceGatheringEnv(grid_size=(args.size, args.size),num_resources=args.num_resources)
+        env = ResourceGatheringEnv(grid_size=(args.size, args.size),num_resources=args.num_resources,seed=args.seed)
     
     np.random.seed(args.seed)
     env.seed(args.seed)